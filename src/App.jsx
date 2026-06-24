dba3d5816eee04b875a853bd72379881ecb7746a
SachinPadyar <sachin@bucketlistt.com>
2026-06-24 11:27:57 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import HeroHome from './Components/HomeRoutes/HeroHome/HeroHome'
+function App() {
+
+  return (
+    <>
+      <HeroHome />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/HomeRoutes/HeroHome/HeroHome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HeroHome.css";
+const HeroHome = () => {
+    return (
+        <div id="hero-home">
+            <div className="BeachTopViewImage">
+                <img src="/Images/IllustrationImages/BeachTopViewImage.png" alt="" />
+            </div>
+            {/* <div>
+                <h1>Kayaking</h1>
+            </div> */}
+        </div>
+    )
+}
+
+export default HeroHome;
